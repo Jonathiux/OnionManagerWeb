877c333f288530f5f9def60a188b24353fb34170
Silvino Aguiar <2121100431@soy.utj.edu.mx>
2023-03-12 23:02:03 -0600

    i anychart

--- cliente/src/pages/Finanzas/index.js
+++ cliente/src/pages/Finanzas/index.js
@@ -1,6 +1,31 @@
 import './index.css'
+import AnyChart from 'anychart-react'
 
 function Finanzas(params) {
+
+    const complexSettings = {
+        width: 800,
+        height: 600,
+        type: 'column',
+        data: "P1,5\nP2,3\nP3,6\nP4,4",
+        title: 'Column chart',
+        yAxis: [1, {
+          orientation: 'right',
+          enabled: true,
+          labels: {
+            format: '{%Value}{decimalPoint:\\,}',
+            fontColor: 'red'
+          }
+        }],
+        legend: {
+          background: 'lightgreen 0.4',
+          padding: 0
+        },
+        lineMarker: {
+          value: 4.5
+        }
+      }
+
     return (
         <>
             <div className="row finanzas-container" >
@@ -34,7 +59,9 @@ function Finanzas(params) {
                         </div>
                         <div className='row grafica-container'>
                             <div className=''>
-                                Grafica
+                                < AnyChart
+                                    {...complexSettings}
+                                />
                             </div>
                             <div className=''>Grafica</div>
                         </div>
