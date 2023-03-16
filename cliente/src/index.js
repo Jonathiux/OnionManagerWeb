582b851df1ffa808105c6ed008058fabde4b72fa
Silvino Aguiar <2121100431@soy.utj.edu.mx>
2023-03-15 23:33:22 -0600

    strict Mode

--- cliente/src/index.js
+++ cliente/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
@@ -7,7 +7,9 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 )
