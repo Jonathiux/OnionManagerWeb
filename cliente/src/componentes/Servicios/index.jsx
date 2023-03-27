235c4fb3128847401f92ff01324eabcc71a42943
Abraham Gonzalez Ruiz <abrahamglezruiz@gmail.com>
2023-03-27 02:32:02 -0600

    Calendario en su funcionamiento completo

--- cliente/src/componentes/Servicios/index.jsx
+++ cliente/src/componentes/Servicios/index.jsx
@@ -24,7 +24,7 @@ function Servicio(){
                     folio={service.folio}
                     imagen={service.folio}
                 />
-            ))}
+            ))} 
         </ServicioLista>
     );
 }
